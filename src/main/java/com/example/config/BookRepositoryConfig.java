fb4fd1d36fcb536774b834e7b504f3da4dae4c4c
sundalei <sundalei@sundaleis-MacBook-Pro.local>
2024-06-02 20:29:17 +0800

    mongodb auth config

--- src/main/java/com/example/config/BookRepositoryConfig.java
+++ src/main/java/com/example/config/BookRepositoryConfig.java
@@ -1,8 +1,9 @@
 package com.example.config;
 
 import com.example.repository.BookRepository;
-import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 
@@ -17,23 +18,32 @@ import java.util.Collection;
 @EnableMongoRepositories(basePackageClasses = BookRepository.class)
 public class BookRepositoryConfig extends AbstractMongoClientConfiguration {
 
+    @SuppressWarnings("null")
     @Override
     protected String getDatabaseName() {
-        return "bookstore";
+        return "mybookstore";
     }
 
+    @SuppressWarnings("null")
     @Override
     protected Collection<String> getMappingBasePackages() {
         return Arrays.asList("com.example.domain");
     }
 
+    @SuppressWarnings("null")
     @Override
     @Bean
     public MongoClient mongoClient() {
         return MongoClients.create(
-            MongoClientSettings.builder()
-            .applyConnectionString(new ConnectionString("mongodb://192.168.64.5:27017,192.168.64.6:27017,192.168.64.7:27017"))
-            .build());
+                MongoClientSettings.builder()
+                        .credential(MongoCredential.createCredential("dalei", "admin", "oarnud9I".toCharArray()))
+                        .applyToClusterSettings(settings -> {
+                            settings.hosts(Arrays.asList(
+                                    new ServerAddress("perconamongo1", 27017),
+                                    new ServerAddress("perconamongo2", 27017),
+                                    new ServerAddress("perconamongo3", 27017)));
+                        })
+                        .build());
 
     }
 }
